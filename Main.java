39492fed069d5a65cf2e24c473959e7455a88c2d
Magdalena Zakrzewska <magdazakrzewskamagda@gmaill.com>
2024-06-01 14:54:52 +0200

    chore: add file

--- Main.java
+++ Main.java
@@ -7,6 +7,7 @@ import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.stage.DirectoryChooser;
 import javafx.stage.Stage;
+
 import java.io.File;
 
 public class Main extends Application {
@@ -32,6 +33,7 @@ public class Main extends Application {
         browseButton.setOnAction(event -> browseDirectory());
 
         Button searchButton = new Button("Search");
+        searchButton.setOnAction(event -> searchFiles());
 
         HBox hBox = new HBox(10, directoryPathField, browseButton);
         VBox vBox = new VBox(10, hBox, searchField, searchButton, resultArea);
@@ -48,6 +50,37 @@ public class Main extends Application {
 
         if (selectedDirectory != null) {
             directoryPathField.setText(selectedDirectory.getAbsolutePath());
+        }
+    }
+
+    private void searchFiles() {
+        String directoryPath = directoryPathField.getText().trim();
+        if (directoryPath.isEmpty()) {
+            resultArea.setText("Please provide a directory path.");
+            return;
+        }
+
+        File directory = new File(directoryPath);
+        if (!directory.isDirectory()) {
+            resultArea.setText("The provided path is not a directory.");
+            return;
+        }
+
+        StringBuilder results = new StringBuilder();
+        listFilesInDirectory(directory, results);
+        resultArea.setText(results.toString());
+    }
+
+    private void listFilesInDirectory(File directory, StringBuilder results) {
+        File[] files = directory.listFiles();
+        if (files != null) {
+            for (File file : files) {
+                if (file.isDirectory()) {
+                    listFilesInDirectory(file, results);
+                } else {
+                    results.append(file.getAbsolutePath()).append("\n");
+                }
+            }
         }
     }
 
